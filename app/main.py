23fb6822c50a8312ddb532ea7cede1e2eba1f0fe
Emilien <emilien.paris@eudiant.univ-lr.fr>
2022-01-15 18:21:00 +0100

    adding CORS Middleware

--- app/main.py
+++ app/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from app.controllers.homeworks import router as HomeworkController
+import fastapi.middleware.cors as cors
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    cors.CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app.include_router(HomeworkController, tags=["Homeworks"], prefix="/homeworks")
 
