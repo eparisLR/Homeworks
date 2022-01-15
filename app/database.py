6c4dde845b7226266e5a55595f6dc5c71f315e46
Emilien <emilien.paris@eudiant.univ-lr.fr>
2022-01-15 18:12:40 +0100

    adding database prod URL

--- app/database.py
+++ app/database.py
@@ -1,7 +1,7 @@
 import os
 import motor.motor_asyncio
 
-MONGO_URL = os.environ.get("MONGODB_URL")
+MONGO_URL = os.environ.get("MONGODB_PROD_URL")
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_URL)
 
